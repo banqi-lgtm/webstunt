5127e596908382a78a290c1851762a9267f02880
banqi-lgtm <wgonzalez@ridepro.co>
2026-04-23 23:48:58 -0500

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,24 @@ import { Toaster } from '@/components/ui/toaster';
 import { Facebook, Instagram } from 'lucide-react';
 
 export const metadata: Metadata = {
-  title: 'CopaStuntF2R_RepuestosNitrox',
-  description: 'Copa Stunt F2R Repuestos Nitrox - Event Registration',
+  title: 'COPASTUNTF2R-NITROX',
+  description: 'Inscripciones Oficiales a la Copa Stunt F2R Repuestos Nitrox. Vive una experiencia increíble.',
+  openGraph: {
+    title: 'COPASTUNTF2R-NITROX',
+    description: 'Inscripciones Oficiales a la Copa Stunt F2R Repuestos Nitrox. Vive una experiencia increíble y participa en la rifa de una MRX 200.',
+    url: 'https://paskinesstunt.com/',
+    siteName: 'Copa Stunt F2R',
+    images: [
+      {
+        url: 'https://img1.wallspic.com/previews/6/0/3/8/28306/28306-motocicleta-stunt_artista_interprete_o_ejecutante-deporte_extremo-las_carreras_de_motos-automovilismo-x750.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'COPASTUNTF2R-NITROX',
+      },
+    ],
+    locale: 'es_CO',
+    type: 'website',
+  },
   icons: {
     icon: '/sponsors/copa stunt nitrox f2r.png',
   },
