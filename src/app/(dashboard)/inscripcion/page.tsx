00bbcbf4feb70ce919150ecc3bbd48215849a07a
banqi-lgtm <wgonzalez@ridepro.co>
2026-07-31 12:38:45 -0500

    fix(inscripcion): fix compilation error caused by duplicate code fragment on selections render block

--- src/app/(dashboard)/inscripcion/page.tsx
+++ src/app/(dashboard)/inscripcion/page.tsx
@@ -1278,8 +1278,6 @@ export default function InscripcionPage() {
 
             </div>
           </div>
-        ) : (v>
-          </div>
         ) : (
           <>
             {/* Botón Volver a Eventos */}
